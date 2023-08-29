82d22b18e6eaad2c1821320e51f567ba84a8adc8
Alex Voigt <107957674+alexmvoigt@users.noreply.github.com>
2023-08-29 11:39:20 -0500

    -Working through grabbing the inputted company number -Moved browser handling to inputs (browser setup is independent of the main method) -Verified SMTP error messages are working

--- WebRegistrar.py
+++ WebRegistrar.py
@@ -13,17 +13,21 @@ from dotenv import dotenv_values
 import smtplib
 from email.message import EmailMessage
 import sys
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 #==================================================
 # INPUTS
 #==================================================
 
 #The auto-generated ID of the account being registered
-accountId = sys.argv[1]
+account_id = sys.argv[1]
 
 #Load sensitive values from .env file
 secrets = dotenv_values("config.env")
+
+#Load webdriver for scraping & inputting
+browser = webdriver.Edge()
 
 
 #==================================================
@@ -33,7 +37,7 @@ secrets = dotenv_values("config.env")
 #Sends alert with error message details on unhandled exceptions via email/SMTP server
 def alert_on_error(e: Exception):
     msg = EmailMessage()
-    msg["Subject"] = "ALERT: Automated registration error for account {accountId}"
+    msg["Subject"] = f"ALERT: Automated registration error for account {account_id}"
     msg["From"] = secrets["ALERT_FROM"]
     msg["To"] = secrets["ALERT_TO"]
     msg.set_content(str(e))
@@ -48,11 +52,19 @@ def alert_on_error(e: Exception):
 #==================================================
 
 def main():
-    try:
-        pass
-    except Exception as e:
-        alert_on_error(e)
+    edit_url = f"https://store.dacotahpaper.com/user/{account_id}/edit"
+    browser.get(edit_url)
+    browser.find_element(By.ID, "edit-name").send_keys(secrets["USERNAME"])
+    browser.find_element(By.ID, "edit-pass").send_keys(secrets["PASSWORD"])
+    browser.find_element(By.ID, "edit-submit").click()
+    company_input = browser.find_element(By.ID, "edit-field-customer-number-und-0-value").get_attribute('value')
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        alert_on_error(e)
+    finally:
+        #browser.quit()
+        pass
         
